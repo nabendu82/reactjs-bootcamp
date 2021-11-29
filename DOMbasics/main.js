dd5d68bd85286db5f6c77d2e4b16b63a98c1b68a
Nabendu Biswas <nabendu.biswas@gmail.com>
2021-11-29 12:04:01 +0530

    Day 3

--- DOMbasics/main.js
+++ DOMbasics/main.js
@@ -6,12 +6,54 @@
 // console.log(document.querySelector('h1'));
 
 //Grab multiple elements
-const nodeItems = document.querySelectorAll('.item');
-const colItems = document.getElementsByClassName('item');
+// const nodeItems = document.querySelectorAll('.item');
+// const colItems = document.getElementsByClassName('item');
 //console.log(document.getElementsByTagName('li'));
 
 //nodeItems.forEach(item => console.log(item));
 //colItems.forEach(item => console.log(item));
-for(var i = 0; i < colItems.length; i++){
-    console.log(colItems[i]);
+// for(var i = 0; i < colItems.length; i++){
+//     console.log(colItems[i]);
+// }
+
+//DOM Manipulation
+// const ul = document.querySelector('.items');
+
+//ul.remove();
+
+
+// const btn = document.querySelector('.btn');
+// btn.style.backgroundColor = 'blue';
+// btn.style.color = 'white';
+
+// btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.backgroundColor = '#ccc';
+//     ul.firstElementChild.textContent = 'HTML';
+//     ul.children[1].innerText = 'CSS';
+//     ul.lastElementChild.innerHTML = '<h4>JavaScript</h4>';
+// })
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+        userList.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
 }
